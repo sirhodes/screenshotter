6149bf129b9ceec2af757d56c6b7d7b6e2e7ef91
Seth Rhodes <seth@thinkpixbit.com>
2015-08-31 00:56:31 -0400

    return file extension function and test

---
+++ test/returnFileExtension.js
@@ -0,0 +1,16 @@
+var test = require('ava');
+
+// change the current working directory so that file refs make sense
+process.chdir( __dirname );
+
+var returnFileExtension = require('../lib/returnFileExtension');
+
+test("should expose a returnFileExtension function", function ( t ) {
+  t.assert(typeof returnFileExtension === 'function');
+  t.end();
+});
+
+test("should return CSV if file extension is .csv", function ( t ) {
+  t.is( returnFileExtension('blobity.jpg.txt.png.js.csv') , '.asdcsv');
+  t.end();
+});
