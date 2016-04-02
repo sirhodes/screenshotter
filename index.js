de7a17ec98f26ac9575d267e3f284209598a08ec
Seth Rhodes <seth@thinkpixbit.com>
2016-04-01 20:45:51 -0400

    index: the magic :tada:

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-module.exports = {};
+const Pageres = require('pageres');
+
+module.exports = takeScreenshot;
+
+function takeScreenshot(url, settings) {
+  return new Promise((resolve, reject) => {
+    settings = settings || {};
+    if (!url) {
+      return reject('You didn\'t supply a URL!');
+    }
+
+    new Pageres({delay: settings.delay || 2})
+      .src(url, settings.devices || ['1024x768'])
+      .dest(settings.dest || __dirname)
+      .run()
+      .then(resolve, reject);
+  });
+}
